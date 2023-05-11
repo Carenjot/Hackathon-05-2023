2c6015d46dc38108f66b18c0787d2d371ea51bd6
Aylin Noiret <aylinndream@hotmail.fr>
2023-05-11 16:38:19 +0200

    Ajout de la description dans le router

--- frontend/src/components/Danemark.jsx
+++ frontend/src/components/Danemark.jsx
@@ -6,11 +6,7 @@
 // //     <div>
 // //       <h1>Le Danemark : Pays des gens heureux </h1>
 // //       {/* <div className="resumer">
-// //         Quel voyage ! Billets pris à la dernière minute, sur un coup de tête et
-// //         nous voilà partis ! Le Danemark est seulement à 2h de vol d'ici,
-// //         direction la capitale Copenhague. La ville n'est pas vraiment dépaysante
-// //         mais ce qui est le plus agréable à première vue : la propreté, le
-// //         respect et le sentiment de sérénité qui y règnent.
+// //
 // //       </div> */}
 // //       <img src="./assets/danemark.jpg" alt="copenhague" />;
 // //     </div>

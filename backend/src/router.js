daa3af8f448ccada088c20a168a50104d3dde3fc
Enzo ANDRE <enzoandre218@gmail.com>
2023-05-11 15:59:25 +0200

    changement fichier

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,71 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const citys = [
+  {
+    id: 1,
+    title: "Voyage à Bruxelles",
+    name: "Thiry Maximilien",
+    photo: {
+      photo1:
+        "https://www.les-voyageuses.net/wp-content/uploads/2017/11/Lesvoyageuses-Belgique-Bruxelles-Guide-4-1280x640.jpg",
+      photo2:
+        "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1300/v1651114332/q4e6bcj35bttqpsdeed8.jpg",
+      photo3:
+        "https://media.routard.com/image/80/0/bruxelles-grand-place.1537800.w630.jpg",
+    },
+    resume:
+      "Découvre la Grand-Place, les musées d'art, les galeries Saint-Hubert, le quartier européen, la Brasserie Cantillon, les moules-frites, les gaufres, Ixelles et le Delirium Café. Bruxelles t'attend !",
+    protips: {
+      protips1: "Les prises éléctrique sont du type C et E",
+      protips2:
+        "Le meilleur moment pour allez à Bruxelles, c'est de Juin à Aout",
+      protips3:
+        "Les langues officiels du pays sont le Français, le Flamand et l'allemand",
+    },
+  },
+  {
+    id: 2,
+    title: "",
+    name: "",
+    photo: "",
+    resume: "",
+    protips: {
+      protips1: "",
+      protips2: "",
+      protips3: "",
+    },
+  },
+  {
+    id: 3,
+    title: "",
+    name: "",
+    photo: "",
+    resume: "",
+    protips: {
+      protips1: "",
+      protips2: "",
+      protips3: "",
+    },
+  },
+  {
+    id: 4,
+    title: "",
+    name: "",
+    photo: "",
+    resume: "",
+    protips: {
+      protips1: "",
+      protips2: "",
+      protips3: "",
+    },
+  },
+];
+
+router.get("/citys", (req, res) => {
+  res.json(citys);
+});
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
